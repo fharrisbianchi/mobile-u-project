5654f01ffd94b8506dfb9a263370b63a5fa2ce6d
HARRIS Francisco <harrisfrancisco@MacBook-Pro-de-Servicedesk-4.local>
2025-10-03 13:25:40 -0300

    Minor adjustment package and app json

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXX",
+  authDomain: "my-app.firebaseapp.com",
+  projectId: "my-app",
+  storageBucket: "my-app.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
